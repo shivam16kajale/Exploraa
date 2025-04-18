f1b5c98f29895d3d88a342c5c22d299459f3bdc6
Shivam Kajale <89018725+shivam16kajale@users.noreply.github.com>
2025-04-18 16:34:55 +0530

    Add files via upload

--- pages/EDA_Visualizer.py
+++ pages/EDA_Visualizer.py
@@ -5,8 +5,33 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.express as px
 
-st.set_page_config(page_title="Exploraa", layout="wide",page_icon="images\Exploraatory-analysis.png")
 
+
+
+st.set_page_config(page_title="Exploraa", layout="wide")
+
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 # --- Sidebar Upload ---
 st.sidebar.title("📂 Upload Your Data")
 uploaded_file = st.sidebar.file_uploader("Upload CSV or Excel file", type=['csv', 'xlsx'])
